12f8346bfee0f4fa867dbf1efb2c96c05caaa209
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-10-02 03:57:15 -0500

    Commited

---
+++ javascript/signup.js
@@ -0,0 +1,8 @@
+var signup = document.querySelector("#signupbut");
+
+function NotYet(){
+    alert(`This Website is Still in Working Progress, You Can't Sign Up Just Yet.`);
+};
+
+
+signup.addEventListener("click", NotYet);
